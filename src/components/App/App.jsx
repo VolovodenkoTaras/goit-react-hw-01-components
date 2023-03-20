b21085b55c9b6a3b22ec6f9ca1c3cd47ba85c377
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-03-20 22:49:00 +0200

    fix2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,26 +7,17 @@ import user from '../../data/user.json';
 import data from '../../data/data.json';
 import friends from '../../data/friends.json';
 import transactions from '../../data/transactions';
+
 import css from './App.module.css';
+
+const { avatar, username, tag, location, stats } = user;
 
 export const App = () => {
   return (
-    <div className={css.app}>
-      <div>
-        <div className={css.wrapper}>
-          <Profile
-            avatar={user.avatar}
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            stats={user.stats}
-          />
-          <FriendList friends={friends} />
-        </div>
-
-        <Statistics title="Upload stats" stats={data} />
-         </div>
-
+    <div className={css.app}>     
+      <Profile avatar={avatar} username={username} tag={tag} location={location} stats={stats}/>
+      <FriendList friends={friends} />
+      <Statistics title="Upload stats" stats={data} />
       <TransactionHistory items={transactions} />
     </div>
   );
